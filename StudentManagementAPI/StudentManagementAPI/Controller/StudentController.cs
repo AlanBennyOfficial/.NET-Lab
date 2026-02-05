65db8b115739ba72f152750eafcd7129655d46d1
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2026-02-05 12:10:01 +0530

    Incomplete code from last week

---
+++ StudentManagementAPI/StudentManagementAPI/Controller/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentManagementAPI.Services;
+
+namespace StudentManagementAPI.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IStudentService _service;
+        public StudentController(IStudentService service)   
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll() => Ok(_service.GetAll());
+        [HttpGet]
+        public IActionResult GetById(int id)
+        {
+            var student = _service.GetById(id);
+            if (student == null)
+                return NotFound();
+            return Ok(student);
+        }
+        [HttpPost]
+        public IActionResult Add(Student student)
+        {
+            var updated = _service.Add(student);
+        }
+    }
+}
